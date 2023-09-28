fb46b84aa827d350d52265b628e66150be17cff6
sreenivas <bandisreenivasulu1122@gmail.com>
2023-09-28 17:20:12 +0530

    ok

---
+++ script.js
@@ -0,0 +1,7 @@
+const mbIcon = document.querySelector('.mb-icon')
+
+
+mbIcon.addEventListener('click', ()=>{
+    const list = document.getElementById('lists')
+    list.classList.toggle('the')
+})
